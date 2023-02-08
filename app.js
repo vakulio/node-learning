1e182809c31c10e1eff01965dbff02d1a2718761
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-02-08 07:36:09 -0800

    add routes folder

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use('/', (req, res, next) => {
-    console.log("Middleware2")
-    res.send("<h1>Hello</h1>")
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
 
 app.listen(3000)
+
+
+
