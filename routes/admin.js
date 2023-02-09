16f40fb4c0dba278df8b17b2d8541b11bd5d7870
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-02-09 04:32:14 -0800

    add pug pages

--- routes/admin.js
+++ routes/admin.js
@@ -7,12 +7,11 @@ const path = require('path')
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+	res.render('add-product', { docTitle: 'Add products' })
 })
 
 router.post('/add-product', (req, res, next) => {
 	products.push({title: req.body.title})
-	console.log(products)
 	res.redirect('/')
 })
 
