1aa80945228368c4916004c8dfc52a382e4ad537
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-02-10 07:01:46 -0800

    add new pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product')
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title)
+    const {title, imageUrl, price, description} = req.body
+    const product = new Product(title, imageUrl, description, price)
     product.save()
     res.redirect('/')
 }
