135a4445249cbbd6ef394467917677a4a654685d
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-02-26 06:53:10 -0800

    sql init

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./utils/database');
 
 const app = express();
 
