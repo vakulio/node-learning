2641a1e28f1a6e6fb44f5f4550492f9158e3f354
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-06-16 23:38:43 +0300

    find a problem

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('647b6b8109e6c803bd85d093')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
