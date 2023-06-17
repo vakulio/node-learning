d07834c451727ab140a3e39fdd7c0eb3e8aa35ea
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-06-17 19:13:53 +0300

    add csrf

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,6 +36,26 @@ app.use(
     store: store
   })
 );
+app.use(csrfProtection)
+
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next()
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+})
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -44,18 +66,6 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
-        User.findOne().then((user) => {
-            if (!user) {
-                const user = new User({
-                    name: 'Vakulio',
-                    email: 'vakulio@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                })
-                user.save()
-            }
-        })
         app.listen(3000, () => {
             console.log(`\x1b[1;34mStart on \u001B[33mhttp://localhost:3000/\u001B[0m`)
         })
