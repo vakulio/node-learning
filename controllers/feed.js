eace3882bb66e7b957da520793aad4e21dacecf7
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-07-11 23:01:14 +0300

    add validator

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,13 +1,19 @@
 exports.getPosts = (req, res) => {
-    res.status(200).json({ title: 'First', content: 'First post content' });
+    res.status(200).json({
+        posts: [{ _id: 1, title: 'First', content: 'First post content', imageUrl: 'images/1.png', creator: { name: "Vakulio" }, createdAt: new Date() }]
+    });
 }
 
 exports.createPosts = (req, res) => {
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({ message: 'Post was created', post: {
-        id: new Date().toISOString(),
-        title: title,
-        content: content
-    } });
+    res.status(201).json({
+        message: 'Post was created', post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: { name: "Vakulio" },
+            createdAt: new Date()
+        }
+    });
 }
